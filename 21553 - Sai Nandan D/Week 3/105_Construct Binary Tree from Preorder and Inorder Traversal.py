02435e173bab48a0413cf3cc033235847f3e3a30
Sai-Nandan-Desetti <80631906+Sai-Nandan-Desetti@users.noreply.github.com>
2022-09-03 21:15:37 +0530

    Update 105_Construct Binary Tree from Preorder and Inorder Traversal.py

--- 21553 - Sai Nandan D/Week 3/105_Construct Binary Tree from Preorder and Inorder Traversal.py
+++ 21553 - Sai Nandan D/Week 3/105_Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -6,9 +6,11 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        # `inorder_map` is used to select the appropriate list of values for the left and the right subtrees, respectively.
         inorder_map = {}
         for i, num in enumerate(inorder):
             inorder_map[num] = i
+        # Start from the left end of `preorder` to build the tree from its root
         self.preorder_index = 0
         
         def build(start, end):
@@ -16,7 +18,8 @@ class Solution:
                 return None
             rootVal = preorder[self.preorder_index]
             self.preorder_index += 1
-            root = TreeNode(rootVal)            
+            root = TreeNode(rootVal)
+            # Build the subtrees using `inorder_map` to get the appropriate "bisection" (It's put in quotes because we aren't actualy bisecting anything.)
             root.left = build(start, inorder_map[rootVal]-1)
             root.right = build(inorder_map[rootVal]+1, end)
             return root
