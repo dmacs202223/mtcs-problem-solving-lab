51d102a10d85e28f21a4468851bb7d3985e3af13
Sai-Nandan-Desetti <80631906+Sai-Nandan-Desetti@users.noreply.github.com>
2022-09-03 21:19:32 +0530

    Update 239_Sliding Window Maximum.py

--- 21553 - Sai Nandan D/Week 3/239_Sliding Window Maximum.py
+++ 21553 - Sai Nandan D/Week 3/239_Sliding Window Maximum.py
@@ -5,10 +5,15 @@ class Solution:
         result, q = [], deque([])
         
         for i, n in enumerate(nums):
+            # Build the queue monotonically
             while q and n > nums[q[-1]]:
                 q.pop()
             q.append(i)
+            
+            # Ensure that a max element does not remain in the queue even after the window has slid past it
             if q[0] == i-k: q.popleft()
+                
+            # The monotonicity of the queue ensures that `q[0]` is the desired max element in the current window
             if i >= k-1: result.append(nums[q[0]])
                 
         return result
