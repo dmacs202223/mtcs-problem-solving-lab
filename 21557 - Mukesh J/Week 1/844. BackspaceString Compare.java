056a663b7dcdddbc38e7e4c24a444e3f3799bf69
Mukesh Jayakodi <75616894+Mukesh0606@users.noreply.github.com>
2022-09-04 12:30:49 +0530

    Update 844. BackspaceString Compare.java

--- 21557 - Mukesh J/Week 1/844. BackspaceString Compare.java
+++ 21557 - Mukesh J/Week 1/844. BackspaceString Compare.java
@@ -1,33 +1,33 @@
-## Question:
+Question:
 Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
 Note that after backspacing an empty text, the text will continue empty.
 
 
-** Example 1: **
+Example 1:
 Input: s = "ab#c", t = "ad#c"
 Output: true
 Explanation: Both s and t become "ac".
 
-** Example 2: **
+ Example 2:
 Input: s = "ab##", t = "c#d#"
 Output: true
 Explanation: Both s and t become "".
 
-** Example 3: **
+Example 3:
 Input: s = "a#c", t = "b"
 Output: false
 Explanation: s becomes "c" while t becomes "b".
  
 
-## Constraints:
+Constraints:
 1 <= s.length, t.length <= 200
 s and t only contain lowercase letters and '#' characters.
 
-## Data Structures: 
+Data Structures: 
 
 String, Stack
 
-## Idea:
+Idea:
 
 1. For first string, keep push elements to stacks and when you see ‘#’ just pop the top of the stack.
 2. Similarly, Do for second string also.
@@ -35,7 +35,7 @@ String, Stack
 4. Time: O(n) where n is the length of the string.
 
 
-## Solution:
+Solution:
 
 class Solution {
     public boolean backspaceCompare(String s, String t) {
