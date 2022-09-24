e28eabd18f49131d1f35054a11d6048f57ad099b
Sai Suraj <53555104+firehawk23@users.noreply.github.com>
2022-09-24 14:31:34 +0530

    Revert "merge mukesh (#18)"
    This reverts commit a714e3c03b14517e5cf3d93e7b054443df7c9a81.

--- 21560 - Sai Suraj/Week 6/215_Kth Largest Element in an Array.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        minh = []
-        for i in nums:
-            heappush(minh,i)
-            if len(minh)>k:
-                heappop(minh)
-        return minh[0]
