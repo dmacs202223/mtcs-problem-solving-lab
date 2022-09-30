92f7999d8f1e6d1d69b752be82bf6843129a961f
Suvam Tamang <90154468+Tamang-Suvam@users.noreply.github.com>
2022-09-30 14:58:44 +0530

    Create 11_Container With Most Water.java

---
+++ 21561 - Suvam Tamang/Week 7/11_Container With Most Water.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int maxarea = 0;
+        int left = 0,right = height.length - 1;
+        
+        while ( left < right ) {
+            maxarea = Math.max(maxarea, Math.min(height[left],height[right]) * (right-left));
+            
+            if(height[left] < height[right]) {
+                left++;
+            } else if (height[left] > height[right]) {
+                right--;
+            } else {
+                left++;
+                right--;
+            }
+        }
+        return maxarea;
+    }
+}
