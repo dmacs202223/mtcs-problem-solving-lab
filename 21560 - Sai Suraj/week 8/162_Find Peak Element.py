e43ca63fd724c6078fdef0659cff9a8217c90891
Sai Suraj <saisuraj2310@gmail.com>
2022-10-15 13:57:24 +0530

    week 8

--- 21560 - Sai Suraj/week 8/162_Find Peak Element.py
+++ 21560 - Sai Suraj/week 8/162_Find Peak Element.py
@@ -2,9 +2,9 @@ class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         l,r=0,len(nums)-1
         while(l<r):
-            m = (l+r)//2
-            if nums[m]>nums[m+1]:
-                r= m
+            mid = (l+r)//2
+            if nums[mid]>nums[mid+1]:
+                r= mid
             else:
-                l=m+1
+                l=mid+1
         return r
