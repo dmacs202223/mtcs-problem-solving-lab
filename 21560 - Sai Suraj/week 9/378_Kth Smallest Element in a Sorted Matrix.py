d644b24495553ec190f3e2d6c4891a1b0241dfcc
Sai Suraj <53555104+firehawk23@users.noreply.github.com>
2023-01-18 11:34:32 +0530

    week 9

--- 21560 - Sai Suraj/week 9/378_Kth Smallest Element in a Sorted Matrix.py
+++ 21560 - Sai Suraj/week 9/378_Kth Smallest Element in a Sorted Matrix.py
@@ -1,1 +1,21 @@
-
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        def countLessorEqual(x):
+            cnt = 0
+            c =len(matrix[0])-1
+            for r in range(len(matrix)):
+                while c >= 0 and matrix[r][c] > x: c -= 1 
+                cnt += (c + 1)
+            return cnt
+        
+        l,r = matrix[0][0],matrix[-1][-1]
+        ans = -1
+        while(l<=r):
+            m = (l+r)//2
+            if countLessorEqual(m) >= k:
+                ans = m
+                r = m-1
+            else:
+                l  =m+1
+            
+        return ans
