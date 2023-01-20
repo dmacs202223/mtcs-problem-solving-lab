63ec51122a8bbc71ec89af851d428ef549bec27f
Mukesh Jayakodi <75616894+Mukesh0606@users.noreply.github.com>
2023-01-20 15:57:36 +0530

    Update 5. Longest Palindromic Substring.java

--- 21557 - Mukesh J/Week10/5.-Longest-Palindromic-Substring/5. Longest Palindromic Substring.java
+++ 21557 - Mukesh J/Week10/5.-Longest-Palindromic-Substring/5. Longest Palindromic Substring.java
@@ -3,15 +3,18 @@ class Solution {
         String result = "";
         int len = 0;
         boolean[][] isPali = new boolean[s.length()][s.length()];
-        for (int i = s.length() - 1; i >= 0; i--) {
-            for (int j = i; j < s.length(); j++) {
-                // System.out.println(i +"   " + j +"   "+s.charAt(i) +"  "+ s.charAt(j));
-                if (s.charAt(i) == s.charAt(j) && (j - i < 2 || isPali[i + 1][j - 1])) {
+
+        for (int i = s.length() - 1; i >= 0; i--) 
+        {
+            for (int j = i; j < s.length(); j++) 
+            {
+                if (s.charAt(i) == s.charAt(j) && (j - i < 2 || isPali[i + 1][j - 1])) 
+                {
                     isPali[i][j] = true;
-                    if (j - i + 1 > len) {
+                    if (j - i + 1 > len) 
+                    {
                         result = s.substring(i, j + 1);
                         len = j - i + 1;
-                        // System.out.println(result +"   " + len);
                     }
                 }
             }
